517a490e03ccc3314245404cee8f7b887528cd16
Austin Jess <austinjess@Austins-MacBook-Pro-2.local>
2018-06-11 10:38:28 -0500

    Added Handle Functions

---
+++ public/data.js
@@ -0,0 +1,10 @@
+const data = [
+  {
+    quote:
+      "Close your eyes. Picture a convict. What's he wearing? Nothing special - baseball cap on backward, baggy pants. He says something ordinary like, 'Yo, that's shizzle'. Okay, now slowly open your eyes again. Who are you picturing? A black man? Wrong. That was a white woman. Surprised? Well, shame on you.",
+    character: 'Michael',
+    paragraphs: 1,
+  },
+];
+
+export default data;
--- src/components/IpsumForm.js
+++ src/components/IpsumForm.js
@@ -3,28 +3,54 @@ import React from 'react';
 class IpsumForm extends React.Component {
   state = {
     character: 'Any',
-    length: '',
-    type: 'Paragraphs'
+    paragraphs: 1,
+    ipsumDisplayed: false
+  };
+
+  handleInputChange = (e) => {
+    const target = e.target;
+    this.setState({ paragraphs: target.value });
+  };
+
+  handleSelectChange = (e) => {
+    const target = e.target;
+    this.setState({ character: target.value });
+  };
+
+  handleSubmit = (e) => {
+    console.log('submitted');
+    console.log(this.state.character);
+    console.log(this.state.paragraphs);
+    e.preventDefault();
+    e.nativeEvent.stopImmediatePropigation();
   };
 
   render() {
     return (
       <div className="ipsumForm">
-        <form>
+        <form onSubmit={this.handleSumbit}>
           <p>Select Your Character</p>
-          <select>
-            <option value="All" selected>
-              All
-            </option>
+          <select
+            value={this.state.character}
+            onChange={this.handleSelectChange}
+            name="characterPick"
+          >
+            <option value="All">All</option>
             <option value="Michael">Michael</option>
             <option value="Dwight">Dwight</option>
             <option value="Jim">Jim</option>
             <option value="Andy">Andy</option>
             <option value="Creed">Creed</option>
           </select>
-          <input type="text" />
-          <input type="radio" value="Words" value="Words" />
-          <input type="radio" value="Paragraphs" value="Paragraphs" />
+          <div className="length">
+            <p>How many paragraphs?</p>
+            <input
+              onChange={this.handleInputChange}
+              name="paragraphLength"
+              type="text"
+            />
+          </div>
+          <input type="submit" value="Bibity Boppity Give Me The Zoppity" />
         </form>
       </div>
     );
